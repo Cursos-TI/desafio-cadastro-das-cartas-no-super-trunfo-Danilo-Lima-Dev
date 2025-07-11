39be47849653af04cd0750c60c54f8a11da47bd9
Danilo-Lima-Dev <dslima.dev@gmail.com>
2025-07-10 22:11:15 -0300

    Update CartasSuperTrunfo.c
    adicionada a formula e variaveis para retornar a informação do PIB per capta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -32,9 +32,14 @@ int main() {
     //Declarei aqui, logo depois da inserção dos dados em suas variaveis
     float densidade1 = (populacao1 / area1);
 
+    
     printf("Digite o PIB (em bilhoes de reais): ");
     scanf("%f", &pib1);
-
+ 
+    //Aqui estou declarando a variavel densidade da carta 1, utilizando o operador matematico de divisão e ja armazenando o valor na variavel
+    //Declarei aqui, logo depois da inserção dos dados em suas variaveis
+    float pibpc1 = (pib1 / populacao1);
+    
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontosturisticos1);
 
@@ -60,9 +65,13 @@ int main() {
     //Aqui estou declarando a variavel densidade da carta 2, utilizando o operador matematico de divisão e ja armazenando o valor na variavel
     //Declarei aqui, logo depois da inserção dos dados em suas variaveis
     float densidade2 = (populacao2 / area2);
-
+    
     printf("Digite o PIB (em bilhoes de reais): ");
     scanf("%f", &pib2);
+    
+        //Aqui estou declarando a variavel densidade da carta , utilizando o operador matematico de divisão e ja armazenando o valor na variavel
+        //Declarei aqui, logo depois da inserção dos dados em suas variaveis
+        float pibpc2 = (pib2 / populacao2);
 
     printf("Digite o numero de pontos turisticos: ");
     scanf("%d", &pontosturisticos2);
@@ -79,6 +88,7 @@ int main() {
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", pontosturisticos1);
     printf("A densidade populacional é de: %.2f\n", densidade1);
+    printf("O PIB Per Capta é de: %.2f\n", pibpc1);
 
     printf("\n"); //Aqui dexei pulando uma linha para ficar mais apresentavel na hora da impressão
 
@@ -91,6 +101,7 @@ int main() {
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontosturisticos2);
     printf("A densidade populacional é de: %.2f\n", densidade2);
+    printf("O PIB Per Capta é de: %.2f\n", pibpc2);
 
     return 0;
 }
